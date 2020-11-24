3954f51af3598601a32a841e61d2892f174dd0fe
consolekakao <console@kakao.com>
2020-11-24 16:29:27 +0900

    Update files

--- function/arrowfunc.js
+++ function/arrowfunc.js
@@ -10,3 +10,13 @@ console.log(result(5, 10));
 var result = (a, b) => a + b;
 
 console.log(result(5, 10));
+
+function result(name, age) {
+  return "당신의 이름은 " + name + "이고 나이는 " + age + "입니다.";
+}
+
+var result = (name, age) => {
+  return "당신의 이름은 " + name + "이고 나이는 " + age + "입니다.";
+};
+
+console.log(result());
