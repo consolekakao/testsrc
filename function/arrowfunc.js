e3da21be25a934da478105bca10e124fa1b8d122
consolekakao <console@kakao.com>
2020-11-24 09:15:12 +0900

    add exampel files

---
+++ function/arrowfunc.js
@@ -0,0 +1,12 @@
+//  ES5
+function result(params1, params2) {
+  return params1 + params2;
+}
+console.log(result(5, 10));
+
+//  ES6
+// var 변수명 = (params) => {}
+
+var result = (a, b) => a + b;
+
+console.log(result(5, 10));
