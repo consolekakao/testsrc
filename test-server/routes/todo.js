6a4debd35badf9e0702b9637d722ce42f8ef8181
consolekakao <console@kakao.com>
2020-12-11 02:22:12 +0900

    Fixed req.body.month.length

--- test-server/routes/todo.js
+++ test-server/routes/todo.js
@@ -3,7 +3,6 @@ var express = require("express");
 var router = express.Router();
 router.use(express.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-
 var mysql = require("mysql");
 const cors = require("cors");
 const dbconnect = require("./dbconnect.json");
@@ -19,15 +18,10 @@ var connection = mysql.createConnection({
 let outdata = [];
 connection.connect();
 router.post("/", function (req, res) {
-  console.log(req.body);
   let lastday = new Date(req.body.year,req.body.month+1,0).getDate();
-  console.log(lastday)
-  console.log(req.body.year+ ' ' +req.body.month);
-  let q = req.body.month;
-  console.log(req.body.month.length)
-  if(req.body.month.length==1) req.body.month = '0'+req.body.month;
-  console.log(`SELECT * FROM calendar where sindex > "${req.body.year}-${req.body.month+1}-01" and sindex < "${req.body.year}-${req.body.month+1}-${lastday}"`)
-  connection.query(`SELECT * FROM calendar where sindex > "${req.body.year}-${req.body.month+1}-01" and sindex < "${req.body.year}-${req.body.month+1}-${lastday}"`, function (err, rows) {
+  let q = req.body.month++;
+  if(req.body.month.toString().length==1) {req.body.month = '0'+req.body.month;}console.log(`SELECT * FROM calendar where sindex > "${req.body.year}-${ req.body.month}-01" and sindex < "${req.body.year}-${req.body.month}-${lastday}"`)
+  connection.query(`SELECT * FROM calendar where sindex > "${req.body.year}-${req.body.month}-01" and sindex < "${req.body.year}-${req.body.month}-${lastday}"`, function (err, rows) {
     try {
       if (err) throw err;
       for (let i = 0; i < rows.length; i++) {
